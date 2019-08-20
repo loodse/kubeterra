866414259e0b54ecb6cd6a386dc753f9c3ec9c3d
Artiom Diomin <artiom@loodse.com>
2019-08-21 01:50:51 +0300

    terraform http backend <-> TerraformState
    close #10
    
    Signed-off-by: Artiom Diomin <artiom@loodse.com>

--- httpbackend/handler.go
+++ httpbackend/handler.go
@@ -17,11 +17,15 @@ package httpbackend
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/go-logr/logr"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	terraformv1alpha1 "github.com/loodse/kubeterra/api/v1alpha1"
@@ -39,22 +43,23 @@ func (h *backendHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "GET":
-		h.getHandler(w, r)
+		h.pullState(w, r)
 	case "POST":
-		h.postHandler(w, r)
+		h.pushState(w, r)
 	case "LOCK":
-		h.lockHandler(w, r)
+		h.lockState(w, r)
 	case "UNLOCK":
-		h.unlockHandler(w, r)
+		h.unlockState(w, r)
 	default:
 		http.NotFound(w, r)
 	}
 }
 
-func (h *backendHandler) getHandler(w http.ResponseWriter, r *http.Request) {
+func (h *backendHandler) pullState(w http.ResponseWriter, _ *http.Request) {
 	state, err := h.getState()
 	if err != nil {
-		http.Error(w, err.msg, err.code)
+		apiErr := extractAPIError(err)
+		http.Error(w, apiErr.msg, apiErr.code)
 		return
 	}
 
@@ -62,42 +67,166 @@ func (h *backendHandler) getHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", state.Spec.State.Raw)
 }
 
-func (h *backendHandler) postHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
+func (h *backendHandler) pushState(w http.ResponseWriter, r *http.Request) {
+	lockID := r.URL.Query().Get("ID")
+	if lockID == "" {
+		http.Error(w, "empty LOCK ID", http.StatusBadRequest)
+		return
+	}
 
-func (h *backendHandler) lockHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
-
-func (h *backendHandler) unlockHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
-
-func (h *backendHandler) getState() (*terraformv1alpha1.TerraformState, *httpError) {
-	state := &terraformv1alpha1.TerraformState{}
-	stateKey := client.ObjectKey{Name: h.name, Namespace: ""}
-	err := h.Get(h.ctx, stateKey, state)
-
+	// TODO: replace ReadAll
+	buf, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		errHttp := &httpError{}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer r.Body.Close()
 
-		switch errAPI := err.(type) {
-		case apierrors.APIStatus:
-			errHttp.code = int(errAPI.Status().Code)
-			errHttp.msg = fmt.Sprintf("%s: %s", errAPI.Status().Reason, errAPI.Status().Message)
-		default:
-			errHttp.code = http.StatusInternalServerError
-			errHttp.msg = err.Error()
+	incomingState := stateModel{}
+	err = json.Unmarshal(buf, &incomingState)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	state, err := h.getState()
+	if err != nil {
+		apiErr := extractAPIError(err)
+		http.Error(w, apiErr.msg, apiErr.code)
+		return
+	}
+
+	existingState := stateModel{}
+	if err := json.Unmarshal(buf, &existingState); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if incomingState.Lineage != existingState.Lineage {
+		http.Error(w, "alien state lineage", http.StatusInternalServerError)
+		return
+	}
+
+	if state.Status.LockID != lockID {
+		http.Error(w, "Locked", http.StatusLocked)
+		return
+	}
+
+	state.Spec.State.Raw = buf
+	// TODO: try to figure out retryable errors and retry
+	if err := h.Update(h.ctx, state); err != nil {
+		apiErr := extractAPIError(err)
+		http.Error(w, apiErr.msg, apiErr.code)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *backendHandler) lockState(w http.ResponseWriter, r *http.Request) {
+	li := lockInfo{}
+	err := json.NewDecoder(r.Body).Decode(&li)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer r.Body.Close()
+
+	state, err := h.getState()
+	if err != nil {
+		apiErr := extractAPIError(err)
+		http.Error(w, apiErr.msg, apiErr.code)
+		return
+	}
+
+	switch {
+	case li.ID == "":
+		http.Error(w, "Unknown lock ID", http.StatusBadRequest)
+	case li.ID != state.Status.LockID:
+		http.Error(w, "LOCKED", http.StatusLocked)
+	case state.Status.LockID == "":
+		now := metav1.Now()
+		state.Status.LockID = li.ID
+		state.Status.LockedSince = &now
+		if err := h.Status().Update(h.ctx, state); err != nil {
+			apiErr := extractAPIError(err)
+			http.Error(w, apiErr.msg, apiErr.code)
+			return
 		}
 
-		return nil, errHttp
+		w.WriteHeader(http.StatusOK)
+	default:
+		http.Error(w, "Unknown state", http.StatusLocked)
+	}
+}
+
+func (h *backendHandler) unlockState(w http.ResponseWriter, r *http.Request) {
+	li := lockInfo{}
+	err := json.NewDecoder(r.Body).Decode(&li)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer r.Body.Close()
+
+	state, err := h.getState()
+	if err != nil {
+		apiErr := extractAPIError(err)
+		http.Error(w, apiErr.msg, apiErr.code)
+		return
+	}
+
+	switch {
+	case li.ID == "":
+		http.Error(w, "Unknown lock ID", http.StatusBadRequest)
+	case li.ID == state.Status.LockID:
+		state.Status.LockID = ""
+		state.Status.LockedSince = nil
+		if err := h.Status().Update(h.ctx, state); err != nil {
+			apiErr := extractAPIError(err)
+			http.Error(w, apiErr.msg, apiErr.code)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	default:
+		http.Error(w, "LOCKED", http.StatusLocked)
+	}
+}
+
+func (h *backendHandler) getState() (*terraformv1alpha1.TerraformState, error) {
+	state := &terraformv1alpha1.TerraformState{}
+	stateKey := client.ObjectKey{Name: h.name, Namespace: ""}
+
+	if err := h.Get(h.ctx, stateKey, state); err != nil {
+		return nil, err
+	}
+
+	if state.Spec.State == nil {
+		return nil, errors.New("state is empty")
 	}
 
 	return state, nil
 }
 
-type tfLockInfoModel struct {
+func extractAPIError(err error) *httpAPIError {
+	apiErr := &httpAPIError{}
+
+	switch errAPI := err.(type) {
+	case apierrors.APIStatus:
+		apiErr.code = int(errAPI.Status().Code)
+		apiErr.msg = fmt.Sprintf("%s: %s", errAPI.Status().Reason, errAPI.Status().Message)
+	case nil:
+		apiErr.code = http.StatusInternalServerError
+		apiErr.msg = "nil error"
+	default:
+		apiErr.code = http.StatusInternalServerError
+		apiErr.msg = err.Error()
+	}
+
+	return apiErr
+}
+
+type lockInfo struct {
 	ID        string
 	Operation string
 	Info      string
@@ -107,13 +236,13 @@ type tfLockInfoModel struct {
 	Path      string
 }
 
-type tfStateModel struct {
+type stateModel struct {
 	Version int    `json:"version"`
 	Lineage string `json:"lineage"`
 	Serial  int    `json:"serial"`
 }
 
-type httpError struct {
+type httpAPIError struct {
 	code int
 	msg  string
 }
